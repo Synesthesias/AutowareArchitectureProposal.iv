3f20925fb626fd43f203e0d01ff6916d409637f2
Yoshimaru Tanaka <robograffitti@gmail.com>
2025-05-27 21:12:27 +0900

    Porting pose_initializer

---
+++ localization/util/pose_initializer/include/pose_initializer/pose_initializer_core.hpp
@@ -0,0 +1,29 @@
+#ifndef POSE_INITIALIZER_CORE_H
+#define POSE_INITIALIZER_CORE_H
+
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+
+class PoseInitializer : public rclcpp::Node
+{
+public:
+  PoseInitializer();
+
+private:
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+};
+
+#endif  // POSE_INITIALIZER_CORE_H
