8ff8ac546ac43ec21e13c647d7dea79d78b49780
Yoshimaru Tanaka <robograffitti@gmail.com>
2025-10-21 12:39:54 +0900

    [humble-devel] porting gnss_poser

--- sensing/preprocessor/gnss/gnss_poser/src/gnss_poser_node.cpp
+++ sensing/preprocessor/gnss/gnss_poser/src/gnss_poser_node.cpp
@@ -13,20 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <ros/ros.h>
+#include <memory>
 
-#include "gnss_poser/gnss_poser_core.h"
+#include <rclcpp/rclcpp.hpp>
+
+#include "gnss_poser/gnss_poser_core.hpp"
 
 int main(int argc, char ** argv)
 {
-  ros::init(argc, argv, "gnss_poser");
-
-  ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
-
-  GNSSPoser::GNSSPoser node(nh, private_nh);
-
-  ros::spin();
-
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<GNSSPoser::GNSSPoser>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
